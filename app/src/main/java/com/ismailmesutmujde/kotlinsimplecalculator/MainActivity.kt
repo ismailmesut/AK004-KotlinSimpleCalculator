7a1d6b41ebedf122d5fa24a8bc3f975c5aaff973
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 12:52:55 +0300

    updated: simple calculator ui design

--- app/src/main/java/com/ismailmesutmujde/kotlinsimplecalculator/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinsimplecalculator/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.ismailmesutmujde.kotlinsimplecalculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinsimplecalculator.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMainActivity : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
+
+
+    }
+
+    fun mySum(view : View) {
+
+    }
+    fun mySub(view : View) {
+
+    }
+    fun myMul(view : View) {
+
+    }
+    fun myDiv(view : View) {
 
     }
 }
